b42c58db2ad07003235ae7b8841abffecd7b0127
knoxiiw <64433273+knoxiiw@users.noreply.github.com>
2020-10-05 13:04:20 -0700

    Create Spring Boot Project

---
+++ Moods/src/test/java/com/moods/MoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
